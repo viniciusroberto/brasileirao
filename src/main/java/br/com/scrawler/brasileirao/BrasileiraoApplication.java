6351368f3f9c75d469269fe6dab1796ad75dacee
someoneviniciusroberto <vinicius.fmu@gmail.com>
2021-04-04 18:44:51 -0300

    Versao inicial

---
+++ src/main/java/br/com/scrawler/brasileirao/BrasileiraoApplication.java
@@ -0,0 +1,13 @@
+package br.com.scrawler.brasileirao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasileiraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasileiraoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/scrawler/brasileirao/BrasileiraoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.scrawler.brasileirao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
